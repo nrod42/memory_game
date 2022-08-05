39c4f6730cad232d8848d3dedb6ff569e0910d13
nrod42 <nigelrod94@gmail.com>
2022-08-05 12:04:47 -0400

    All styling done. Max of 6 columns display with grid autofill. Sound plays and muted when clicked and does not affect anything else.

--- src/App.js
+++ src/App.js
@@ -1,60 +1,81 @@
 import "./index.css";
-import React, { useState, useEffect } from "react";
-import { characters, randomize} from "./Components/Characters"
+import React, { useState, useEffect, useRef } from "react";
+import { characters, randomize } from "./Components/Characters";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Card from "./Components/Card";
+import theme from "./audio/The_Simpsons_cut.mp3";
 
 const App = () => {
+  const [shuffledCards, setShuffledCards] = useState([]);
   const [score, setScore] = useState(0);
   const [best, setBest] = useState(0);
   const [guessed, setGuessed] = useState([]);
+  const [isPlaying, setPlaying] = useState(false);
 
-  useEffect(() => {
-    if (score > best) setBest(score)
-    if (score >= characters.length) {
-      resetGame()
-    }
-  }, [score, best]);
+  const themeRef = useRef(new Audio(theme));
+
+  const shuffle = () => {
+    setShuffledCards(
+      randomize().map((character) => (
+        <Card
+          key={character.id}
+          character={character}
+          handleClick={handleClick}
+        />
+      ))
+    );
+  };
 
   const resetGame = () => {
     setGuessed([]);
     setScore(0);
-  }
+  };
 
   const handleClick = (id) => {
-    setGuessed((prevState) => [...prevState, id])
+    setGuessed((prevState) => [...prevState, id]);
     if (guessed.includes(id)) {
-      resetGame()
+      resetGame();
     } else {
       setScore((prevState) => prevState + 1);
     }
   };
 
-  let theme = new Audio("/audio/The_Simpsons.mp3")
-  const playTheme = () => {
-    theme.play()
-  }
+  const toggleSound = () => {
+    themeRef.current.play();
+    setPlaying((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    if (score > best) setBest(score);
+    if (score >= characters.length) {
+      resetGame();
+    }
+  }, [score, best]);
+
+  useEffect(() => {
+    if (isPlaying === true) {
+      themeRef.current.volume = 1;
+      themeRef.current.loop = true;
+    } else {
+      themeRef.current.volume = 0;
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    shuffle();
+  }, [guessed]);
 
   return (
     <div className="App">
-      <Header score={score} best={best}/>
-      <button className={"playBtn"} onClick={playTheme}>
-        <img
-          src={window.location.origin + "/img/volume.svg"}
-          alt="sound on">
-        </img>
-      </button>
+      <Header
+        score={score}
+        best={best}
+        toggleSound={toggleSound}
+        isPlaying={isPlaying}
+      />
       <main>
-          <div className="cardGrid">
-            {randomize().map((character) => (
-              <Card
-                key={character.id}
-                character={character}
-                handleClick={handleClick}
-              />
-            ))}
-          </div>
+        <div className="cardGrid">{shuffledCards}</div>
       </main>
       <Footer />
     </div>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,24 @@
 import React from "react";
 
 const Header = (props) => {
-  const { score, best } = props;
+  const { score, best, toggleSound, isPlaying } = props;
   return (
     <header className="header">
+      <div className="spacerDiv"></div>
       <h1>The Simpsons Memory Game</h1>
       <div className="score">
         <p className="bestScore">Best Score: {best}</p>
         <p>Current Score: {score}</p>
       </div>
+      <button className={"playBtn"} onClick={toggleSound}>
+        <img
+          src={
+            window.location.origin +
+            (isPlaying ? "/img/volume.svg" : "/img/mute.svg")
+          }
+          alt={isPlaying ? "sound on button" : "mute button"}
+        ></img>
+      </button>
     </header>
   );
 };
