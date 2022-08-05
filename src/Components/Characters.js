b13d475bfbb5959b222e1fe298e9c665fc54deee
nrod42 <nigelrod94@gmail.com>
2022-08-05 15:15:39 -0400

    gh pages fix

--- src/Components/Characters.js
+++ src/Components/Characters.js
@@ -1,54 +1,58 @@
 import uniqid from "uniqid";
-import Homer from "../img/Homer.png";
-import Marge from "../img/Marge.png";
-import Bart from "../img/Bart.png";
-import Lisa from "../img/Lisa.png";
-import Maggie from "../img/Maggie.png";
-import Grandpa from "../img/Grandpa.png";
-import Krusty from "../img/Krusty.png";
-import Edna from "../img/Edna.png";
-import Barney from "../img/Barney.png";
-import Apu from "../img/Apu.png";
-import Milhouse from "../img/Milhouse.png";
-import Moe from "../img/Moe.png";
-import Burns from "../img/Burns.png";
-import Flanders from "../img/Flanders.png";
-import Ralph from "../img/Ralph.png";
-import Skinner from "../img/Skinner.png";
-import Smithers from "../img/Smithers.png";
-import Nelson from "../img/Nelson.png";
-import Comic from "../img/Comic.png";
-import Willie from "../img/Willie.png";
-import Wiggum from "../img/Wiggum.png";
-import Lenny from "../img/Lenny.webp";
-import Lou from "../img/Lou.webp";
-import Carl from "../img/Carl.webp";
+// // import Homer from "../img/Homer.png";
+// import Marge from "../img/Marge.png";
+// import Bart from "../img/Bart.png";
+// import Lisa from "../img/Lisa.png";
+// import Maggie from "../img/Maggie.png";
+// import Grandpa from "../img/Grandpa.png";
+// import Krusty from "../img/Krusty.png";
+// import Edna from "../img/Edna.png";
+// import Barney from "../img/Barney.png";
+// import Apu from "../img/Apu.png";
+// import Milhouse from "../img/Milhouse.png";
+// import Moe from "../img/Moe.png";
+// import Burns from "../img/Burns.png";
+// import Flanders from "../img/Flanders.png";
+// import Ralph from "../img/Ralph.png";
+// import Skinner from "../img/Skinner.png";
+// import Smithers from "../img/Smithers.png";
+// import Nelson from "../img/Nelson.png";
+// import Comic from "../img/Comic.png";
+// import Willie from "../img/Willie.png";
+// import Wiggum from "../img/Wiggum.png";
+// import Lenny from "../img/Lenny.webp";
+// import Lou from "../img/Lou.webp";
+// import Carl from "../img/Carl.webp";
 
 const characters = [
-  { id: uniqid(), name: "Homer", img: Homer },
-  { id: uniqid(), name: "Marge", img: Marge },
-  { id: uniqid(), name: "Bart", img: Bart },
-  { id: uniqid(), name: "Lisa", img: Lisa },
-  { id: uniqid(), name: "Maggie", img: Maggie },
-  { id: uniqid(), name: "Grandpa", img: Grandpa },
-  { id: uniqid(), name: "Krusty", img: Krusty },
-  { id: uniqid(), name: "Edna", img: Edna },
-  { id: uniqid(), name: "Barney", img: Barney },
-  { id: uniqid(), name: "Apu", img: Apu },
-  { id: uniqid(), name: "Milhouse", img: Milhouse },
-  { id: uniqid(), name: "Moe", img: Moe },
-  { id: uniqid(), name: "Mr. Burns", img: Burns },
-  { id: uniqid(), name: "Ned Flanders", img: Flanders },
-  { id: uniqid(), name: "Ralph", img: Ralph },
-  { id: uniqid(), name: "Principal Skinner", img: Skinner },
-  { id: uniqid(), name: "Mr. Smithers", img: Smithers },
-  { id: uniqid(), name: "Nelson", img: Nelson },
-  { id: uniqid(), name: "Comic Book Guy", img: Comic },
-  { id: uniqid(), name: "Willie", img: Willie },
-  { id: uniqid(), name: "Chief Wiggum", img: Wiggum },
-  { id: uniqid(), name: "Lenny", img: Lenny },
-  { id: uniqid(), name: "Lou", img: Lou },
-  { id: uniqid(), name: "Carl", img: Carl },
+  { id: uniqid(), name: "Homer", img: require("../img/Homer.png") },
+  { id: uniqid(), name: "Marge", img: require("../img/Marge.png") },
+  { id: uniqid(), name: "Bart", img: require("../img/Bart.png") },
+  { id: uniqid(), name: "Lisa", img: require("../img/Lisa.png") },
+  { id: uniqid(), name: "Maggie", img: require("../img/Maggie.png") },
+  { id: uniqid(), name: "Grandpa", img: require("../img/Grandpa.png") },
+  { id: uniqid(), name: "Krusty", img: require("../img/Krusty.png") },
+  { id: uniqid(), name: "Edna", img: require("../img/Edna.png") },
+  { id: uniqid(), name: "Barney", img: require("../img/Barney.png") },
+  { id: uniqid(), name: "Apu", img: require("../img/Apu.png") },
+  { id: uniqid(), name: "Milhouse", img: require("../img/Milhouse.png") },
+  { id: uniqid(), name: "Moe", img: require("../img/Moe.png") },
+  { id: uniqid(), name: "Mr. Burns", img: require("../img/Burns.png") },
+  { id: uniqid(), name: "Ned Flanders", img: require("../img/Flanders.png") },
+  { id: uniqid(), name: "Ralph", img: require("../img/Ralph.png") },
+  {
+    id: uniqid(),
+    name: "Principal Skinner",
+    img: require("../img/Skinner.png"),
+  },
+  { id: uniqid(), name: "Mr. Smithers", img: require("../img/Smithers.png") },
+  { id: uniqid(), name: "Nelson", img: require("../img/Nelson.png") },
+  { id: uniqid(), name: "Comic Book Guy", img: require("../img/Comic.png") },
+  { id: uniqid(), name: "Willie", img: require("../img/Willie.png") },
+  { id: uniqid(), name: "Chief Wiggum", img: require("../img/Wiggum.png") },
+  { id: uniqid(), name: "Lenny", img: require("../img/Lenny.webp") },
+  { id: uniqid(), name: "Lou", img: require("../img/Lou.webp") },
+  { id: uniqid(), name: "Carl", img: require("../img/Carl.webp") },
 ];
 
 // Randomly choose 12 characters from the character set
